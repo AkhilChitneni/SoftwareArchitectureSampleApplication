64448117c538a47daebfe4ca8dc969f78b77c68b
Akhil Chitneni <akhilchitneni@gmail.com>
2022-04-13 19:16:30 -0400

    docs changes

---
+++ docs/html/search/classes_7.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['session_0',['Session',['../class_automation_a_p_i_1_1_session.html',1,'AutomationAPI']]],
+  ['spherebuilder_1',['SphereBuilder',['../class_automation_a_p_i_1_1_sphere_builder.html',1,'AutomationAPI']]],
+  ['spherebuilderimpl_2',['SphereBuilderImpl',['../class_automation_a_p_i_1_1_sphere_builder_impl.html',1,'AutomationAPI']]]
+];
---
+++ docs/html/search/functions_6.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['save_0',['Save',['../class_automation_a_p_i_1_1_part.html#a3c3ecd459c51189b79639eae97923216',1,'AutomationAPI::Part']]],
+  ['settype_1',['SetType',['../class_automation_a_p_i_1_1_sphere_builder.html#a4c1f72fa0d3d9df87a1de19ed314741b',1,'AutomationAPI::SphereBuilder']]]
+];
